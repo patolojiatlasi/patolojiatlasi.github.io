37209bc3614fdcffea4a6d47c3b7b9db59beb96f
sbalci <drserdarbalci@gmail.com>
2022-03-20 18:07:13 +0300

    update

---
+++ localActions.sh
@@ -0,0 +1,6 @@
+# sh '/Users/serdarbalci/Documents/GitHub/patolojiatlasi.github.io/localActions.sh'
+act --secret-file ~/my.secrets
+git add .
+git commit -m "WIP local render added changes `date +'%Y-%m-%d %H:%M:%S'`" # WIP ifadesi eklenirse github actions çalışmaz.
+# git commit -m "local render added changes `date +'%Y-%m-%d %H:%M:%S'`"
+# git push origin main
