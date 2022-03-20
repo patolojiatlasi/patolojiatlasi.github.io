afdd2ace7c88c9d3e14d5d0cbbdaa9923fb7e6d5
sbalci <drserdarbalci@gmail.com>
2022-03-20 19:16:34 +0300

    WIP local render added changes 2022-03-20 19:16:34

--- localActions.sh
+++ localActions.sh
@@ -1,6 +1,17 @@
 # sh '/Users/serdarbalci/Documents/GitHub/patolojiatlasi.github.io/localActions.sh'
+Rscript -e 'if (!requireNamespace("xfun", quietly = TRUE)) {install.packages("xfun")}'
+Rscript -e 'if (!requireNamespace("quarto", quietly = TRUE)) {install.packages("quarto")}'
+Rscript -e 'if (!requireNamespace("fs", quietly = TRUE)) {install.packages("fs")}'
+Rscript -e 'fs::file_copy(path = "./_quarto_EN.yml", new_path = "./_quarto.yml", overwrite = TRUE)'
+Rscript -e 'fs::file_copy(path = "./R/languageEN.R", new_path = "./R/language.R", overwrite = TRUE)'
+Rscript -e 'quarto::quarto_render(".", as_job = FALSE)'
+Rscript -e 'if (file.exists("./EN/CNAME")){fs::file_delete(path = "./EN/CNAME")}'
+Rscript -e 'fs::file_copy(path = "./_quarto_TR.yml", new_path = "./_quarto.yml", overwrite = TRUE)'
+Rscript -e 'fs::file_copy(path = "./R/languageTR.R", new_path = "./R/language.R", overwrite = TRUE)'
+Rscript -e 'quarto::quarto_render(".", as_job = FALSE)'
+Rscript -e 'fs::dir_copy(path = "./EN", new_path = "./docs/EN", overwrite = TRUE)'
 act --secret-file ~/my.secrets
 git add .
 git commit -m "WIP local render added changes `date +'%Y-%m-%d %H:%M:%S'`" # WIP ifadesi eklenirse github actions çalışmaz.
-# git commit -m "local render added changes `date +'%Y-%m-%d %H:%M:%S'`"
-# git push origin main
+git commit -m "local render added changes `date +'%Y-%m-%d %H:%M:%S'`"
+git push origin main
