d41a44ff185585720f8449bac6cb61b90fe4f479
sbalci <drserdarbalci@gmail.com>
2022-03-22 21:25:26 +0300

    updates

--- localActions.sh
+++ localActions.sh
@@ -11,7 +11,7 @@ Rscript -e 'if (file.exists("./EN/CNAME")){fs::file_delete(path = "./EN/CNAME")}
 Rscript -e 'fs::file_copy(path = "./_quarto_TR.yml", new_path = "./_quarto.yml", overwrite = TRUE)'
 Rscript -e 'fs::file_copy(path = "./R/languageTR.R", new_path = "./R/language.R", overwrite = TRUE)'
 Rscript -e 'quarto::quarto_render(".", as_job = FALSE)'
-Rscript -e 'fs::dir_copy(path = "./EN", new_path = "./docs/EN", overwrite = TRUE)'
+# Rscript -e 'fs::dir_copy(path = "./EN", new_path = "./docs/EN", overwrite = TRUE)'
 # act --secret-file ~/my.secrets
 # git add .
 # git commit -m "WIP local render added changes `date +'%Y-%m-%d %H:%M:%S'`" # WIP ifadesi eklenirse github actions çalışmaz.
