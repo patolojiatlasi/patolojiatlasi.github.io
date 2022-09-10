dcf1560ce31855c53968988b7eb6a9f128119275
sbalci <drserdarbalci@gmail.com>
2022-09-11 00:18:57 +0300

    WIP

--- R/tweet-random-cases.R
+++ R/tweet-random-cases.R
@@ -1,9 +1,11 @@
-if (!requireNamespace("jsonlite", quietly = TRUE)) {install.packages("jsonlite")}
+if (!requireNamespace("jsonlite", quietly = TRUE)) {install.packages("jsonlite", dependencies = TRUE, quiet = TRUE, verbose = FALSE)}
 library(jsonlite, quietly = TRUE, warn.conflicts = FALSE, verbose = FALSE)
-if (!requireNamespace("magrittr", quietly = TRUE)) {install.packages("magrittr")}
+if (!requireNamespace("magrittr", quietly = TRUE)) {install.packages("magrittr", dependencies = TRUE, quiet = TRUE, verbose = FALSE)}
 library(magrittr, quietly = TRUE, warn.conflicts = FALSE, verbose = FALSE)
-if (!requireNamespace("dplyr", quietly = TRUE)) {install.packages("dplyr")}
+if (!requireNamespace("dplyr", quietly = TRUE)) {install.packages("dplyr", dependencies = TRUE, quiet = TRUE, verbose = FALSE)}
 library(dplyr, quietly = TRUE, warn.conflicts = FALSE, verbose = FALSE)
+if (!requireNamespace("glue", quietly = TRUE)) {install.packages("glue", dependencies = TRUE, quiet = TRUE, verbose = FALSE)}
+library(glue, quietly = TRUE, warn.conflicts = FALSE, verbose = FALSE)
 
 searchcontent <- jsonlite::fromJSON(txt = "./docs/search.json", simplifyDataFrame = TRUE) %>% as.data.frame()
 searchcontent <- searchcontent %>% dplyr::filter(!is.na(section))
