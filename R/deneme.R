a819a44fc4f2ff26bd0e9089d2365636ed2b877e
sbalci <drserdarbalci@gmail.com>
2023-05-06 23:00:43 +0300

    lecture notes are being prepared

--- R/deneme.R
+++ R/deneme.R
@@ -3,8 +3,8 @@ render_file <- purrr::safely(function(file) {rmarkdown::render(file, encoding = 
 purrr::map(.x = filestorender, .f= render_file)
 
 
-filestorender <- list.files(path = "./lecture-notes/", pattern = ".qmd", full.names = TRUE, recursive = TRUE)
-purrr::map(.x = filestorender, .f= quarto::quarto_render)
+filestorender <- list.files(path = "./lecture-notes", pattern = ".qmd", full.names = TRUE, recursive = TRUE)
+purrr::map(.x = filestorender[1:10], .f= quarto::quarto_render, output_format = "all")
 # quarto::quarto_render(input = "./lecture-notes/*.qmd")
 
 quarto::quarto_render("./lecture-notes/alt-gis-tumorleri-sunum-2.qmd", output_format = "all")
@@ -26,3 +26,46 @@ purrr::map(
 
 quarto::quarto_render(input = "./lecture-notes/alt-gis-tumorleri-sunum-2.qmd",
                       output_format = "all")
+
+
+
+
+filestorender <- list.files(path = "./lecture-notes/", pattern = ".qmd", full.names = TRUE, recursive = TRUE)
+
+# file.exists(filestorender)
+
+purrr::map(.x = filestorender,
+           .f = quarto::quarto_render,
+           output_format = "gfm"
+           )
+
+quarto::quarto_render(input = "./lecture-notes/pulmonary-infections.qmd",
+                      output_format = "gfm")
+
+
+
+purrr::map2( .x = filestorender,
+             .y = c("revealjs", "gfm"),
+             .f = quarto::quarto_render(input = .x, output_format = .y)
+)
+
+
+
+md_files <- list.files(path = './lecture-notes', pattern = '*\\.md', full.names = FALSE, recursive = TRUE)
+
+md_files <- md_files[!grepl(pattern = "README", x = md_files)]
+
+md_files <- gsub(pattern = "\\.md", replacement = "", x = md_files)
+
+md_files_list <- purrr::map(
+  .x = md_files,
+  .f = ~ paste0(
+    "[", .x , "](./lecture-notes/", .x , ".md)\n"
+  )
+)
+
+readme_text <- paste0(unlist(md_files_list), collapse = "\n\n")
+
+readme_text <- paste0("# Patoloji Ders Notları \n\n", readme_text)
+
+writeLines(text = readme_text, con = "./lecture-notes/README.md")
---
+++ lecture-notes/congenital-malformations-and-perinatal-brain-injury.md
@@ -0,0 +1,12 @@
+# Title
+Serdar Balcı
+
+<img
+src="img%5CCongenital-Malformations-and-Perinatal-Brain-Injury0.png"
+class="quarto-discovered-preview-image" />
+
+**Robbins Basic Pathology**
+
+![](img%5CCongenital-Malformations-and-Perinatal-Brain-Injury1.png)
+
+**Robbins Basic Pathology**
