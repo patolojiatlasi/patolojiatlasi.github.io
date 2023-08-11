3593e8f0237cecf53be7386c38028f2d92c1701f
sbalci <drserdarbalci@gmail.com>
2023-08-11 08:54:50 +0300

    WIP

--- R/quick-render.R
+++ R/quick-render.R
@@ -1,7 +1,7 @@
-source(./R/render_epub_word_TR)
-source(./R/render_pdf_TR)
-source(./R/render_epub_word_EN)
-source(./R/render_pdf_EN)
+source("./R/render_epub_word_TR.R")
+source("./R/render_pdf_TR.R")
+source("./R/render_epub_word_EN.R")
+source("./R/render_pdf_EN.R")
 
 
 
