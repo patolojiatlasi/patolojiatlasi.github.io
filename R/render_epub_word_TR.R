178c7b550687d93ef82bad501c1f0895fc2d33c5
sbalci <drserdarbalci@gmail.com>
2023-09-14 22:34:33 +0300

    WIP

--- R/render_epub_word_TR.R
+++ R/render_epub_word_TR.R
@@ -20,47 +20,57 @@ if (dir.exists(paths = "./_freeze_TR_epub_word")) {
                overwrite = TRUE)
 }
 
-patolojiatlasi_histopathologyatlas <- readxl::read_excel("./patolojiatlasi_histopathologyatlas.xlsx", sheet = "chapters")
-
-patolojiatlasi_histopathologyatlas <- patolojiatlasi_histopathologyatlas[, c("TR_chapter_qmd", "TR_epub_word_chapter_qmd")]
-
-patolojiatlasi_histopathologyatlas$TR_chapter_qmd <- paste0(patolojiatlasi_histopathologyatlas$TR_chapter_qmd, ".qmd")
-
-patolojiatlasi_histopathologyatlas$TR_epub_word_chapter_qmd <- paste0(patolojiatlasi_histopathologyatlas$TR_epub_word_chapter_qmd, ".qmd")
 
 
-fs::file_copy(path = patolojiatlasi_histopathologyatlas$TR_chapter_qmd,
-              new_path = patolojiatlasi_histopathologyatlas$TR_epub_word_chapter_qmd,
+chapters <- list.files(path = ".", pattern = ".qmd", recursive = FALSE)
+
+chapters <- paste0("./", chapters)
+
+chapters_epub_word_TR <- gsub(pattern = ".qmd", replacement = "_epub_word_TR.qmd", x = chapters)
+
+subchapters <- list.files(path = "./_subchapters", pattern = ".qmd", recursive = FALSE)
+
+subchapters <- paste0("./_subchapters/", subchapters)
+
+subchapters_epub_word_TR <- gsub(pattern = ".qmd", replacement = "_epub_word_TR.qmd", x = subchapters)
+
+all_chapters <- c(chapters, subchapters)
+
+all_chapters_epub_word_TR <- c(chapters_epub_word_TR, subchapters_epub_word_TR)
+
+
+fs::file_copy(path = all_chapters,
+              new_path = all_chapters_epub_word_TR,
               overwrite = TRUE)
 
 
-qmd_epub_word_TR_files <- list.files(path = ".", pattern = "./*_epub_word_TR.qmd", recursive = FALSE)
+# qmd_epub_word_TR_files <- list.files(path = ".", pattern = "./*_epub_word_TR.qmd", recursive = FALSE)
+#
+# subchapter_files <- list.files(path = "./_subchapters", pattern = "*.qmd", recursive = FALSE)
+#
+# subchapter_files <- paste0("./_subchapters/", subchapter_files)
+#
+# qmd_epub_word_TR_files <- c(qmd_epub_word_TR_files, subchapter_files)
 
-subchapter_files <- list.files(path = "./_subchapters", pattern = "*.qmd", recursive = FALSE)
 
-subchapter_files <- paste0("./_subchapters/", subchapter_files)
-
-qmd_epub_word_TR_files <- c(qmd_epub_word_TR_files, subchapter_files)
-
-
-xfun::gsub_files(files = qmd_epub_word_TR_files,
+xfun::gsub_files(files = all_chapters_epub_word_TR,
                  pattern = "panel-tabset",
                  replacement = "")
 
-xfun::gsub_files(files = qmd_epub_word_TR_files,
+xfun::gsub_files(files = all_chapters_epub_word_TR,
                  pattern = ":::::",
                  replacement = "")
 
 
-xfun::gsub_files(files = qmd_epub_word_TR_files,
+xfun::gsub_files(files = all_chapters_epub_word_TR,
                  pattern = "#+\\s*WSI - Link",
                  replacement = "")
 
-xfun::gsub_files(files = qmd_epub_word_TR_files,
+xfun::gsub_files(files = all_chapters_epub_word_TR,
                  pattern = "#+\\s*WSI",
                  replacement = "")
 
-xfun::gsub_files(files = qmd_epub_word_TR_files,
+xfun::gsub_files(files = all_chapters_epub_word_TR,
                  pattern = "#+\\s*Diagnosis",
                  replacement = "")
 
@@ -68,7 +78,7 @@ xfun::gsub_files(files = qmd_epub_word_TR_files,
 #                  pattern = '::: {.callout-tip collapse="true" appearance="default" icon="true"}',
 #                  replacement = "")
 
-xfun::gsub_files(files = qmd_epub_word_TR_files,
+xfun::gsub_files(files = all_chapters_epub_word_TR,
                  pattern = "#+\\s*Tanı için tıklayın",
                  replacement = "### Tanı")
 
@@ -79,12 +89,12 @@ xfun::gsub_files(files = qmd_epub_word_TR_files,
 
 
 
-xfun::gsub_files(files = qmd_epub_word_TR_files,
+xfun::gsub_files(files = all_chapters_epub_word_TR,
                  pattern = '\\!\\[\\]\\(\\.\\/qrcodes\\/\\{\\{template\\}\\}-\\{\\{stain\\}\\}_qrcode.svg\\)\\{width="15%"\\}',
                  replacement = "")
 
 
-xfun::gsub_files(files = qmd_epub_word_TR_files,
+xfun::gsub_files(files = all_chapters_epub_word_TR,
                  pattern = ".qmd >}}",
                  replacement = "_epub_word_TR.qmd >}}"
 )
@@ -113,20 +123,23 @@ if (dir.exists(paths = "./_freeze")) {
 }
 
 
+fs::file_copy(path = "./_quarto_TR.yml",
+              new_path = "./_quarto.yml",
+              overwrite = TRUE)
 
 
-patolojiatlasi_histopathologyatlas <- readxl::read_excel("./patolojiatlasi_histopathologyatlas.xlsx", sheet = "chapters")
+# patolojiatlasi_histopathologyatlas <- readxl::read_excel("./patolojiatlasi_histopathologyatlas.xlsx", sheet = "chapters")
+#
+# patolojiatlasi_histopathologyatlas <- patolojiatlasi_histopathologyatlas[, c("TR_chapter_qmd", "TR_epub_word_chapter_qmd")]
+#
+# '%>%' <- magrittr:::`%>%`
+#
+# patolojiatlasi_histopathologyatlas <- patolojiatlasi_histopathologyatlas %>%
+#   dplyr::distinct() %>%
+#   dplyr::filter(TR_chapter_qmd != TR_epub_word_chapter_qmd)
+#
+# patolojiatlasi_histopathologyatlas$TR_epub_word_chapter_qmd <- paste0(patolojiatlasi_histopathologyatlas$TR_epub_word_chapter_qmd, ".qmd")
 
-patolojiatlasi_histopathologyatlas <- patolojiatlasi_histopathologyatlas[, c("TR_chapter_qmd", "TR_epub_word_chapter_qmd")]
-
-'%>%' <- magrittr:::`%>%`
-
-patolojiatlasi_histopathologyatlas <- patolojiatlasi_histopathologyatlas %>%
-  dplyr::distinct() %>%
-  dplyr::filter(TR_chapter_qmd != TR_epub_word_chapter_qmd)
-
-patolojiatlasi_histopathologyatlas$TR_epub_word_chapter_qmd <- paste0(patolojiatlasi_histopathologyatlas$TR_epub_word_chapter_qmd, ".qmd")
-
-fs::file_delete(path = patolojiatlasi_histopathologyatlas$TR_epub_word_chapter_qmd)
+fs::file_delete(path = all_chapters_epub_word_TR)
 
 
